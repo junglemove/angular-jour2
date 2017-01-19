72439e2040534d702f1efc3a119b27ca11704b9e
Thibaut VISTICOT <thibaut.visticot@gmail.com>
2017-01-19 16:12:10 +0100

    Exo1: PIPE devise

--- src/app/Pipes/devise.pipe.ts
+++ src/app/Pipes/devise.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DevisePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+    return value + " $";
   }
 
 }
